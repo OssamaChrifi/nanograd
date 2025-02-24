81c0d37ef1327f27a04912527c8bbe850e76ba07
Ossama CHRIFI <OChrifi@slb.com>
2025-02-24 13:29:29 -0500

    Add 2D Convolution layer

--- autograd/modules/conv.py
+++ autograd/modules/conv.py
@@ -8,9 +8,10 @@ class Conv2d(Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: Union[int, tuple], **kwargs):
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.kernel_size = kernel_size
+        self.kernel_size = kernel_size if isinstance(kernel_size, int) else kernel_size[0]
         self.weight = Tensor(
-            np.random.randn(out_channels, in_channels, kernel_size, kernel_size) * np.sqrt(2. / in_channels),
+            np.random.randn(out_channels, in_channels, kernel_size, kernel_size) * \
+                np.sqrt(2. / (in_channels * kernel_size * kernel_size)),
             requires_grad=True
         )
         self.bias = Tensor(
@@ -27,37 +28,35 @@ class Conv2d(Module):
         return self._conv2d(x, self.weight, self.bias, self.padding, self.stride, self.dilation, self.groups)
     
     def _conv2d(self, x: Tensor, weight: Tensor, bias: Tensor, padding: int, stride: int, dilation: int, groups: int):
-        batch_size, in_channels, height, width = x.data.shape
+        batch_size, in_channels, _, _ = x.data.shape
         out_channels, _, kernel_size, _ = weight.data.shape
-        if (in_channels != self.in_channels) | (x.data.ndim != 4):
+        if (in_channels != self.in_channels) or (x.data.ndim != 4):
             raise ValueError(f"Expected input of shape (batch_size, {self.in_channels}, height, width), but got {x.data.shape}")
-        if kernel_size != self.kernel_size:
-            raise ValueError(f"Expected kernel of size {self.kernel_size}, but got {kernel_size}")
         if groups != 1:
             raise NotImplementedError("Groups > 1 is not supported yet")
-        if stride != 1:
-            raise NotImplementedError("Stride > 1 is not supported yet")
-        if dilation != 1:
-            raise NotImplementedError("Dilation > 1 is not supported yet")
-        if padding != 0:
-            raise NotImplementedError("Padding > 0 is not supported yet")
         if self.padding_mode != 'zeros':
             raise NotImplementedError("Padding mode != 'zeros' is not supported yet")
-        x_unf = self._im2col(x, kernel_size, stride, padding)
+        x_unf, out_height, out_width = self._im2col(x, kernel_size, stride, padding, dilation)
         weight_unf = weight.data.reshape(out_channels, -1)
-        out_unf = x_unf @ weight_unf.T + bias.data
-        out = out_unf.reshape(batch_size, out_channels, height, width)
+        out_unf = x_unf.transpose(0,2,1) @ weight_unf.T + bias.data
+        out = out_unf.transpose(0,2,1).reshape(batch_size, out_channels, out_height, out_width)
         return Tensor(out)
     
-    def _im2col(self, x: Tensor, kernel_size: int, stride: int, padding: int):
-        batch_size, in_channels, height, width = x.data.shape
-        out_height = (height + 2 * padding - kernel_size) // stride + 1
-        out_width = (width + 2 * padding - kernel_size) // stride + 1
-        x_unf = np.zeros((batch_size, in_channels, kernel_size, kernel_size, out_height, out_width))
-        for i in range(kernel_size):
-            for j in range(kernel_size):
-                x_unf[:, :, i, j, :, :] = x.data[:, :, i:i + out_height * stride:stride, j:j + out_width * stride:stride]
-        return x_unf.reshape(batch_size, in_channels * kernel_size * kernel_size, -1)
-    
+    def _im2col(self, x: Tensor, kernel_size: int, stride: int, padding: int, dilation: int):
+        batch_size = x.data.shape[0]
+        x_padded = np.pad(x.data, ((0,0), (0,0), (padding, padding), (padding, padding)), mode='constant')
+
+        kernel_effective = dilation * (kernel_size - 1) + 1
+        out_height = (x_padded.shape[2] - kernel_effective) // stride + 1
+        out_width = (x_padded.shape[3] - kernel_effective) // stride + 1
+        
+        windows = np.lib.stride_tricks.sliding_window_view(x_padded, (kernel_effective, kernel_effective), axis=(2, 3))
+        windows = windows[:, :, ::stride, ::stride, :, :]
+        windows = windows[:, :, :, :, ::dilation, ::dilation]
+
+        col = windows.transpose(0, 2, 3, 1, 4, 5)  # (batch_size, out_height, out_width, in_channels, kernel_size, kernel_size)
+        col = col.reshape(batch_size, out_height * out_width, -1).transpose(0, 2, 1)  # (batch_size, in_channels*kernel_size*kernel_size, out_height*out_width)
+        return col, out_height, out_width
+
     def parameters(self):
         return [self.weight, self.bias]
