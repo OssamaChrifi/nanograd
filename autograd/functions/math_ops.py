Fix backpropagation for conv2d

autograd/functions/math_ops.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Tuple, Optional
 from .base import Function
+from .helper import im2col, col2im
 
 class Add(Function):
     def forward(self, x, y) -> np.ndarray:
@@ -102,4 +103,54 @@ class Log(Function):
     def backward(self, grad_output : Optional[np.ndarray]) -> np.ndarray:
         x = self.input
         return grad_output / x.data
-    
+
+class Conv2d(Function):
+    def forward(self, x, weight, bias, padding, stride, dilation, groups, padding_mode) -> np.ndarray:
+        self.input = (x, weight, bias)
+        self.ctx = (x, weight, bias, padding, stride, dilation, groups, padding_mode)
+        batch_size, in_channels, _, _ = x.data.shape
+        out_channels = weight.data.shape[0]
+        if groups == 1:
+            x_unf, out_height, out_width = im2col(x.data, weight.data, padding, stride, dilation, padding_mode)
+            out_channels = weight.data.shape[0]
+            weight_unf = weight.data.reshape(out_channels, -1)
+            out_unf = x_unf.transpose(0, 2, 1) @ weight_unf.T + bias.data
+            out = out_unf.transpose(0, 2, 1).reshape(x.data.shape[0], out_channels, out_height, out_width)
+            self.x_unf = x_unf
+            return out
+        
+        if in_channels % groups != 0 or out_channels % groups != 0:
+            raise ValueError("in_channels and out_channels must be divisible by groups")
+        in_channels_per_group = in_channels // groups
+        out_channels_per_group = out_channels // groups
+        outputs = []
+
+        for g in range(groups):
+            x_group = x.data[:, g * in_channels_per_group : (g + 1) * in_channels_per_group, :, :]
+            x_unf_group, out_height, out_width = im2col(x_group, weight.data, padding, stride, dilation, padding_mode)
+            weight_group = weight.data[g * out_channels_per_group : (g + 1) * out_channels_per_group, :, :, :]
+            weight_unf_group = weight_group.reshape(out_channels_per_group, -1)
+            bias_group = bias.data[g * out_channels_per_group : (g + 1) * out_channels_per_group]
+            out_unf_group = x_unf_group.transpose(0,2,1) @ weight_unf_group.T + bias_group
+            out_group = out_unf_group.transpose(0,2,1).reshape(batch_size, out_channels_per_group, out_height, out_width)
+            outputs.append(out_group)
+
+        out = np.concatenate(outputs, axis=1)
+        return out
+
+    def backward(self, grad_output: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        x, weight, bias, padding, stride, dilation, groups, padding_mode = self.ctx
+        batch_size = x.data.shape[0]
+        out_channels, in_channels, k_h, k_w = weight.data.shape
+        grad_bias = grad_output.sum(axis=(0, 2, 3))
+        grad_output_reshaped = grad_output.reshape(batch_size, out_channels, -1)
+        grad_weight_unf = np.zeros((out_channels, in_channels * k_h * k_w))
+        for i in range(batch_size):
+            grad_weight_unf += grad_output_reshaped[i] @ self.x_unf[i].T
+        grad_weight = grad_weight_unf.reshape(weight.data.shape)
+        grad_x_unf = np.zeros_like(self.x_unf)
+        weight_unf = weight.data.reshape(out_channels, -1)
+        for i in range(batch_size):
+            grad_x_unf[i] = (grad_output_reshaped[i].T @ weight_unf).T
+        grad_x = col2im(grad_x_unf, x.data.shape, (k_h, k_w), padding, stride, dilation)
+        return grad_x, grad_weight, grad_bias

autograd/modules/conv.py
@@ -33,79 +33,13 @@ class Conv2d(Module):
         self.padding_mode = kwargs.get('padding_mode', 'zeros')
 
     def forward(self, x: Tensor) -> Tensor:
-        return self._conv2d(x, self.weight, self.bias, self.padding, self.stride, self.dilation, self.groups)
-    
-    def backward(self, grad_output: np.ndarray) -> Tensor:
-        cols, out_height, out_width = self._im2col(self.x_padded, self.kernel_size[0], self.kernel_size[1], self.stride)
-        grad_filters = grad_output.reshape(self.batch_size, self.out_channels, -1) @ cols.T
-        self.weight.grad = grad_filters.reshape(self.weight.shape)
-        self.bias.grad = grad_output.sum(axis=(0, 2, 3))
-        grad_input = grad_output.transpose(1, 0, 2, 3).reshape(self.out_channels, -1) @ self.filters.data.reshape(self.out_channels, -1)
-        grad_input = self._col2im(grad_input, self.x.shape, out_height, out_width)
-        return grad_input
-    
-    def _conv2d(self, x: Tensor, weight: Tensor, bias: Tensor, padding: int, stride: int, dilation: int, groups: int) -> Tensor:
         if x.data.ndim == 3:
             x.data.unsqueeze(0)
-        batch_size, in_channels, _, _ = x.data.shape
-        out_channels, _, kernel_size_h, kernel_size_w = weight.data.shape
+        in_channels = x.data.shape[1]
         if (in_channels != self.in_channels) or (x.data.ndim != 4):
             raise ValueError(f"Expected input of shape (batch_size, {self.in_channels}, height, width), but got {x.data.shape}")
-        if self.padding_mode not in self.PAD_MODES:
-            raise NotImplementedError(f"Padding mode {self.padding_mode} is not supported")
-        if groups == 1:
-            x_unf, out_height, out_width = self._im2col(x, kernel_size_h, kernel_size_w, stride, padding, dilation)
-            weight_unf = weight.data.reshape(out_channels, -1)
-            out_unf = x_unf.transpose(0,2,1) @ weight_unf.T + bias.data
-            out = out_unf.transpose(0,2,1).reshape(batch_size, out_channels, out_height, out_width)
-            return Tensor(out)
-        if in_channels % groups != 0 or out_channels % groups != 0:
-            raise ValueError("in_channels and out_channels must be divisible by groups")
-        in_channels_per_group = in_channels // groups
-        out_channels_per_group = out_channels // groups
-        outputs = []
-
-        for g in range(groups):
-            x_group_data = x.data[:, g * in_channels_per_group : (g + 1) * in_channels_per_group, :, :]
-            x_group = Tensor(x_group_data)
-            x_unf_group, out_height, out_width = self._im2col(x_group, kernel_size_h, kernel_size_w, stride, padding, dilation)
-            weight_group = weight.data[g * out_channels_per_group : (g + 1) * out_channels_per_group, :, :, :]
-            weight_unf_group = weight_group.reshape(out_channels_per_group, -1)
-            bias_group = bias.data[g * out_channels_per_group : (g + 1) * out_channels_per_group]
-            out_unf_group = x_unf_group.transpose(0,2,1) @ weight_unf_group.T + bias_group
-            out_group = out_unf_group.transpose(0,2,1).reshape(batch_size, out_channels_per_group, out_height, out_width)
-            outputs.append(out_group)
-
-        out = np.concatenate(outputs, axis=1)
-        result = Tensor(out)
-        result._ctx = self
-        return result
+        return x.conv2d(self.weight, self.bias, self.padding, self.stride, self.dilation, self.groups)
     
-    def _im2col(self, x: Tensor, kernel_size_h: int, kernel_size_w: int, stride: int, padding: int, dilation: int) -> Tensor:
-        batch_size = x.data.shape[0]
-        mode, kwargs = self.PAD_MODES[self.padding_mode]
-        x_padded = np.pad(x.data, ((0,0), (0,0), (padding, padding), (padding, padding)), mode=mode, **kwargs)
-        kernel_effective_h = dilation * (kernel_size_h - 1) + 1
-        kernel_effective_w = dilation * (kernel_size_w - 1) + 1
-        out_height = (x_padded.shape[2] - kernel_effective_h) // stride + 1
-        out_width = (x_padded.shape[3] - kernel_effective_w) // stride + 1
-        windows = np.lib.stride_tricks.sliding_window_view(x_padded, (kernel_effective_h, kernel_effective_w), axis=(2, 3))
-        windows = windows[:, :, ::stride, ::stride, :, :]
-        windows = windows[:, :, :, :, ::dilation, ::dilation]
-        col = windows.transpose(0, 2, 3, 1, 4, 5)  
-        col = col.reshape(batch_size, out_height * out_width, -1).transpose(0, 2, 1)  
-        return col, out_height, out_width
-    
-    def _col2im(self, cols, x_shape, out_h, out_w):
-        batch_size, channels, h, w = x_shape
-        k_h, k_w = self.kernel_size
-        h_padded, w_padded = h + 2*self.padding, w + 2*self.padding
-        x_padded = np.zeros((batch_size, channels, h_padded, w_padded))
-        for y in range(k_h):
-            for x in range(k_w):
-                x_padded[:, :, y:y + self.stride*out_h:self.stride, x:x + self.stride*out_w:self.stride] += cols[:, :, y, x, :, :]
-        return x_padded[:, :, self.padding:-self.padding, self.padding:-self.padding]
-
     @property
     def parameters(self):
         return [self.weight, self.bias]

autograd/tensor.py
@@ -1,7 +1,7 @@
 import numpy as np
 from typing import Optional
 from .functions import Add, Sub, Mul, Div, Pow, MatMul, \
-    Sum, Mean, Exp, Log
+    Sum, Mean, Exp, Log, Conv2d
 
 class Tensor:
     """
@@ -84,6 +84,10 @@ class Tensor:
     
     def log(self) -> 'Tensor':
         return Log.apply(self)
+    
+    def conv2d(self, weight : 'Tensor', bias : 'Tensor', padding : int = 0, stride : int = 1, \
+               dilation : int = 1, groups : int = 1, padding_mode: str = 'zeros') -> 'Tensor':
+        return Conv2d.apply(self, weight, bias, padding, stride, dilation, groups, padding_mode)
 
     def backward(self, grad_output : Optional[np.ndarray] = None) -> None:
         if grad_output is None: