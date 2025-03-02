Fix backpropagation for Linear Module

autograd/functions/math_ops.py
@@ -8,8 +8,21 @@ class Add(Function):
         return x.data + y.data
     
     def backward(self, grad_output : Optional[np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
-        return grad_output, grad_output
+        _, y = self.input
+        grad_x = grad_output
+        grad_y = grad_output
+        shape_y = y.data.shape
+        shape_out = grad_output.shape
 
+        if shape_y != shape_out:
+            diff = len(shape_out) - len(shape_y)
+            new_shape_y = (1,) * diff + shape_y
+            axes = tuple(i for i, (dim_out, dim_y) in enumerate(zip(shape_out, new_shape_y)) if dim_y == 1)
+            grad_y = grad_output.sum(axis=axes, keepdims=True)
+            grad_y = grad_y.reshape(shape_y)
+
+        return grad_x, grad_y
+    
 class Sub(Function):
     def forward(self, x, y) -> np.ndarray:
         self.input = (x, y)

autograd/modules/conv.py
@@ -38,7 +38,7 @@ class Conv2d(Module):
     def backward(self, grad_output: np.ndarray) -> Tensor:
         cols, out_height, out_width = self._im2col(self.x_padded, self.kernel_size[0], self.kernel_size[1], self.stride)
         grad_filters = grad_output.reshape(self.batch_size, self.out_channels, -1) @ cols.T
-        self.filters.grad = grad_filters.reshape(self.filters.shape)
+        self.weight.grad = grad_filters.reshape(self.weight.shape)
         self.bias.grad = grad_output.sum(axis=(0, 2, 3))
         grad_input = grad_output.transpose(1, 0, 2, 3).reshape(self.out_channels, -1) @ self.filters.data.reshape(self.out_channels, -1)
         grad_input = self._col2im(grad_input, self.x.shape, out_height, out_width)